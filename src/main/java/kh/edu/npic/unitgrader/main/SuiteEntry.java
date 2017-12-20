9b86cdd0ee666279b1563e5a1d0ca8262701076f
Joshua A. Horton <joshua_horton@sil.org>
2017-12-20 15:37:53 +0700

    Starts proper implementation of the SuiteEntry class.

--- src/main/java/kh/edu/npic/unitgrader/main/SuiteEntry.java
+++ src/main/java/kh/edu/npic/unitgrader/main/SuiteEntry.java
@@ -1,10 +1,33 @@
 package kh.edu.npic.unitgrader.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kh.edu.npic.unitgrader.util.console.EndMenuOption;
+import kh.edu.npic.unitgrader.util.console.NumericalMenu;
+import kh.edu.npic.unitgrader.util.console.Option;
+
 public class SuiteEntry {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		class GradeOption extends Option {
+			GradeOption() {
+				super("Grade student assignments.");
+			}
+			
+			public boolean function() {
+				AutogradeDriver.main(args);
+			
+				return true;
+			};
+		}
+		
+		List<Option> options = new ArrayList<Option>();
+		options.add(new GradeOption());
+		options.add(new EndMenuOption("Quit."));
+		
+		NumericalMenu menu = new NumericalMenu(options);
+		menu.run();
 	}
 
 }
