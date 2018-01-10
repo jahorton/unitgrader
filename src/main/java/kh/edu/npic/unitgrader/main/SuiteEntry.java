ff04e4cc8520aa7f911a858a966415771470dec3
Joshua A. Horton <joshua_horton@sil.org>
2018-01-10 15:46:29 +0700

    Adds control paths to all three modules from SuiteEntry.java.

--- src/main/java/kh/edu/npic/unitgrader/main/SuiteEntry.java
+++ src/main/java/kh/edu/npic/unitgrader/main/SuiteEntry.java
@@ -7,6 +7,7 @@ import kh.edu.npic.unitgrader.define.TestSpecWriter;
 import kh.edu.npic.unitgrader.util.console.EndMenuOption;
 import kh.edu.npic.unitgrader.util.console.NumericalMenu;
 import kh.edu.npic.unitgrader.util.console.Option;
+import kh.edu.npic.unitgrader.verify.VerifyUnitTestWriter;
 
 public class SuiteEntry {
 
@@ -35,9 +36,22 @@ public class SuiteEntry {
 			};
 		}
 		
+		class CreateVerifierOption extends Option {
+			CreateVerifierOption() {
+				super("Generate a verifier for a specified class file.");
+			}
+			
+			public boolean function() {
+				VerifyUnitTestWriter.main(args);
+			
+				return true;
+			};
+		}
+		
 		List<Option> options = new ArrayList<Option>();
 		options.add(new GradeOption());
 		options.add(new TestWriteOption());
+		options.add(new CreateVerifierOption());
 		options.add(new EndMenuOption("Quit."));
 		
 		NumericalMenu menu = new NumericalMenu(options);
